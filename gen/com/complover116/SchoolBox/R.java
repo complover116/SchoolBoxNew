27de7049c45395eaf7560dd2f224b1e12521e4d4
Vladimir <complover116@gmail.com>
2013-04-03 15:35:29 +0400

    Кнопочки!

--- gen/com/complover116/SchoolBox/R.java
+++ gen/com/complover116/SchoolBox/R.java
@@ -18,18 +18,18 @@ public final class R {
     }
     public static final class id {
         public static final int PB1=0x7f080010;
-        public static final int PRC=0x7f08000c;
-        public static final int QR=0x7f08000b;
+        public static final int PRC=0x7f08000d;
+        public static final int QR=0x7f08000c;
         public static final int Question=0x7f080000;
         public static final int Spinner=0x7f080011;
         public static final int TextView=0x7f080012;
-        public static final int VidVi=0x7f08000e;
+        public static final int VidVi=0x7f08000f;
         public static final int ans1=0x7f080002;
         public static final int ans2=0x7f080001;
         public static final int ans3=0x7f080003;
         public static final int ans4=0x7f080009;
-        public static final int button1=0x7f08000f;
-        public static final int mediaController1=0x7f08000d;
+        public static final int button1=0x7f08000b;
+        public static final int mediaController1=0x7f08000e;
         public static final int menu_settings=0x7f080014;
         public static final int progress=0x7f080005;
         public static final int textView1=0x7f080006;
