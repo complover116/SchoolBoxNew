611b8ee3bb1bfab10c33c2e46e9fb284a34ae4dc
complover116 <complover116@gmail.com>
2013-04-12 10:02:33 +0400

    Adding a type parameter to Question.java + cleanup

--- src/com/complover116/SchoolBox/Question.java
+++ src/com/complover116/SchoolBox/Question.java
@@ -1,28 +1,20 @@
 package com.complover116.SchoolBox;
 
 public class Question {
- String question = "Произошла ошибка. Обьявлено количество вопросов большее, чем их есть на самом деле. Возможно, нажатие на ответ 1 исправит её.";
- String pans1 = "Нажмите сюда";
+ String question = "An error occured. Please click answer 1. It will countas a right answer."
+ String pans1 = "CLICK ME"
  String pans2 = "";
  String pans3 = "";
  String pans4 = "";
+ String type = "";
  int rans = 1;
-	public Question(String q, String pan1, String pan2, String pan3, String pan4, int rians) {
+	public Question(String q, String pan1, String pan2, String pan3, String pan4, int rians, String typ) {
 		question =q;
-		// TODO Auto-generated constructor stub
 		pans1 = pan1;
 		pans2 = pan2;
 		pans3 = pan3;
 		pans4 = pan4;
 		rans = rians;
+		type = typ
 	}
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
 }
