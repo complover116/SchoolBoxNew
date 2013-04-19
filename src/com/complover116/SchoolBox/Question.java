51317cc22bb1e3ad369e4e1bee1b5d244bf8ce15
complover116 <complover116@gmail.com>
2013-04-19 10:58:56 +0400

    Решил поменять систему классификации - теперь в файле будет число, а не строка.

--- src/com/complover116/SchoolBox/Question.java
+++ src/com/complover116/SchoolBox/Question.java
@@ -1,14 +1,14 @@
 package com.complover116.SchoolBox;
 
 public class Question {
- String question = "An error occured. Please click answer 1. It will countas a right answer."
+ String question = "An error occured. Please click answer 1. It will count as a right answer."
  String pans1 = "CLICK ME"
  String pans2 = "";
  String pans3 = "";
  String pans4 = "";
- String type = "";
+ int type;
  int rans = 1;
-	public Question(String q, String pan1, String pan2, String pan3, String pan4, int rians, String typ) {
+	public Question(String q, String pan1, String pan2, String pan3, String pan4, int rians, int typ) {
 		question =q;
 		pans1 = pan1;
 		pans2 = pan2;
