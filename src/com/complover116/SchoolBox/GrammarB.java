e03ba8dec2802e1f1fb1b74d05624fbf479c6686
complover116 <complover116@gmail.com>
2013-05-23 10:04:35 +0400

    Update GrammarB.java

--- src/com/complover116/SchoolBox/GrammarB.java
+++ src/com/complover116/SchoolBox/GrammarB.java
@@ -1,5 +1,22 @@
 package com.complover116.SchoolBox;
 
-public class GrammarB
-{
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class GrammarB extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent pootis = new Intent(this, GrammarTestResult.class);
+        startActivity(pootis);
+    }
+    public void onBackPressed() {
+    	
+    }
 }
